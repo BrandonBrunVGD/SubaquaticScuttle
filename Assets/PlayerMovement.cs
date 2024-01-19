ee0775f2a9567b351cf3c6aaefd0af69ca77ec18
Brandon Brun <118461262+BrandonBrunVGD@users.noreply.github.com>
2024-01-18 22:21:10 -0400

    v0.1.1
    added player shooting

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,11 +7,22 @@ public class PlayerMovement : MonoBehaviour {
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform playerPos;
     [SerializeField] private float mMoveSpeed = 10f;
-
+    [SerializeField] private GameObject bullet;
     void FixedUpdate()
     {
         Vector3 mInput = new Vector3(0, Input.GetAxisRaw("Vertical"), 0);
 
         rb.MovePosition(transform.position + mInput * Time.deltaTime * mMoveSpeed);
+
+        
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown("space")) {
+            SpawnBullet();
+        }
+    }
+    private void SpawnBullet() {
+        GameObject spawnObject = Instantiate(bullet, playerPos.position, Quaternion.identity);
     }
 }
